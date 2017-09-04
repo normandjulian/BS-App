c671285990a4a257466c117aa9f816c1eea295b3
normandjulian <normandjulian@gmail.com>
2017-09-04 22:19:12 +0200

    continue on register page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -9,115 +9,114 @@ import {LoginService} from "./login-service";
 import {RegisterPage} from "../register/register";
 
 @Component({
-    selector: 'page-login',
-    templateUrl: 'login.html',
-    providers: [LoginService]
+  selector: 'page-login',
+  templateUrl: 'login.html',
+  providers: [LoginService]
 })
 
 export class LoginPage implements OnInit {
-    public user: Guest;
-    public isLogged: Boolean = false;
-    public login_form: any = null;
-    public toStore: boolean = false;
+  public user: Guest;
+  public isLogged: Boolean = false;
+  public login_form: any = null;
+  public toStore: boolean = false;
 
-    constructor(private navCtrl: NavController,
-                private storage: Storage,
-                private alertCtrl: AlertController,
-                private fb: FormBuilder,
-                private modalCtrl: ModalController,
-                private navParams: NavParams,
-                private service: LoginService,
-                private bs: BackStatProvider) {
-    }
+  constructor(private navCtrl: NavController,
+              private storage: Storage,
+              private alertCtrl: AlertController,
+              private fb: FormBuilder,
+              private modalCtrl: ModalController,
+              private navParams: NavParams,
+              private service: LoginService,
+              private bs: BackStatProvider) {
+  }
 
-    /**
-     * Display a notification to the layout
-     * @param  {string} message [The message to display]
-     */
-    notification(message: string): void {
-        this.alertCtrl.create({
-            title: 'Information',
-            subTitle: message,
-            buttons: ['OK']
-        }).present();
-    }
+  /**
+   * Display a notification to the layout
+   * @param  {string} message [The message to display]
+   */
+  notification(message: string): void {
+    this.alertCtrl.create({
+      title: 'Information',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
+  }
 
-    /**
-     * Login to the application
-     * @param  {Guest}  guest [The pair login/password]
-     */
-    sign_in(guest: Guest): void {
-        this.service.sign_in(guest).subscribe(
-            (token: Token) => {
-                if (token['error']) {
-                    this.notification(token['message']);
-                } else {
-                    this.storage.set('credits', JSON.stringify(token));
-                    this.bs.set_token(token.token);
-                    this.navCtrl.setRoot(DashboardPage);
-                }
-            },
-            err => console.log(err)
-        );
-    }
-
-    /**
-     * Redirect to the page Register
-     */
-    goto_registerPage(): void {
-        this.modalCtrl.create(RegisterPage).present();
-    }
-
-    save_credits() {
-        // TODO
-    }
-
-    lf_credits_storage() {
-        this.storage.get('credits').then(res => {
-            if (res) {
-                let user = JSON.parse(res);
-                this.sign_in({
-                    'email': user.email,
-                    'password': user.password
-                });
-            }
-        });
-    }
-
-    /**
-     * Initialisation of the page
-     */
-    ngOnInit() {
-        this.login_form = this.fb.group({
-            email: ['', [<any>Validators.required]],
-            password: ['', [<any>Validators.required]]
-        });
-
-        // Check the storage for auto signin
-        // this.lf_credits_storage();
-
-        // Get the email from the RegisterPage
-        if (typeof this.navParams.get('email') !== 'undefined') {
-            this.login_form.setValue({
-                email: this.navParams.get('email')
-            });
+  /**
+   * Login to the application
+   * @param  {Guest}  guest [The pair login/password]
+   */
+  sign_in(guest: Guest): void {
+    this.service.sign_in(guest).subscribe(
+      (token: Token) => {
+        if (token['error']) {
+          this.notification(token['message']);
+        } else {
+          this.storage.set('credits', JSON.stringify(token));
+          this.bs.set_token(token.token);
+          this.navCtrl.setRoot(DashboardPage);
         }
+      },
+      err => console.log(err)
+    );
+  }
 
-        // this.sign_in({
-        //     email: 'normandjulian@gmail.com',
-        //     password: 'juju'
-        // });
-        this.goto_registerPage();
+  /**
+   * Redirect to the page Register
+   */
+  goto_registerPage(): void {
+    this.modalCtrl.create(RegisterPage).present();
+  }
+
+  save_credits() {
+    // TODO
+  }
+
+  lf_credits_storage() {
+    this.storage.get('credits').then(res => {
+      if (res) {
+        let user = JSON.parse(res);
+        this.sign_in({
+          'email': user.email,
+          'password': user.password
+        });
+      }
+    });
+  }
+
+  /**
+   * Initialisation of the page
+   */
+  ngOnInit() {
+    this.login_form = this.fb.group({
+      email: ['', [<any>Validators.required]],
+      password: ['', [<any>Validators.required]]
+    });
+
+    // Check the storage for auto signin
+    // this.lf_credits_storage();
+
+    // Get the email from the RegisterPage
+    if (typeof this.navParams.get('email') !== 'undefined') {
+      this.login_form.setValue({
+        email: this.navParams.get('email')
+      });
     }
+
+    this.sign_in({
+      email: 'normandjulian@gmail.com',
+      password: 'juju'
+    });
+  }
 }
 
 class Token {
-    email: string;
-    token: string;
-    _id: string;
+  email: string;
+  token: string;
+  _id: string;
 }
 
 class Guest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ViewController, AlertController} from 'ionic-angular';
+import {ViewController, AlertController, ToastController} from 'ionic-angular';
 import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 import {RegisterService} from './register-service';
@@ -8,88 +8,91 @@ import {RegisterUser} from '../../classes/user.class';
 import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
-    selector: 'register-page',
-    templateUrl: 'register.html',
-    providers: [RegisterService]
+  selector: 'register-page',
+  templateUrl: 'register.html',
+  providers: [RegisterService]
 })
 export class RegisterPage implements OnInit {
-    public clubs: Club[] = [];
-    public register_form: FormGroup;
+  public clubs: Club[] = [];
+  public register_form: FormGroup;
 
-    constructor(public viewCtrl: ViewController,
-                public registerService: RegisterService,
-                private fb: FormBuilder,
-                public alertCtrl: AlertController) {
+  constructor(public viewCtrl: ViewController,
+              public toastCtrl: ToastController,
+              public registerService: RegisterService,
+              private fb: FormBuilder,
+              public alertCtrl: AlertController) {
 
-        // Define the form
-        this.register_form = fb.group({
-            email: ['', [<any>Validators.required]],
-            lastname: ['', []],
-            firstname: ['', []],
-            password: ['', [<any>Validators.required]],
-            confirm_pwd: ['', [<any>Validators.required]],
-            club_id: ['', [<any>Validators.required]]
-        });
-    };
+    // Define the form
+    this.register_form = fb.group({
+      email: ['', [<any>Validators.required]],
+      lastname: ['', []],
+      firstname: ['', []],
+      password: ['', [<any>Validators.required]],
+      confirm_pwd: ['', [<any>Validators.required]],
+      club_id: ['', [<any>Validators.required]]
+    });
+  };
 
-    /**
-     * Let the user register to our application
-     * @param  {RegisterUser} value   The form
-     * @param  {boolean} isValid Is the form valid
-     */
-    register(value: RegisterUser, isValid: boolean): void {
-        if (RegisterPage.validate_password(value.password, value.confirm_pwd)) {
-            this.registerService.sign_up(value).subscribe(
-                res => {
-                    if (res['message']) {
-                    } else {
-                        this.viewCtrl.dismiss();
-                    }
-                },
-                (err: HttpErrorResponse) => {
-                    if (err.error instanceof Error) {
-                        // A client-side or network error occurred. Handle it accordingly.
-                        console.log('An error occurred:', err.error.message);
-                    } else {
-                        // The backend returned an unsuccessful response code.
-                        // The response body may contain clues as to what went wrong,
-                        this.notification('Error', `Message : ${err.error.code} <br/> Message : ${err.error.errmsg}`);
-                        console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
-                    }
-                }
-            );
-        } else {
-            this.notification('Information', 'Vos mots de passes ne correspondent pas');
+  /**
+   * Let the user register to our application
+   * @param  {RegisterUser} value   The form
+   * @param  {boolean} isValid Is the form valid
+   */
+  register(value: RegisterUser, isValid: boolean): void {
+    if (RegisterPage.validate_password(value.password, value.confirm_pwd)) {
+      this.registerService.sign_up(value).subscribe(
+        () => {
+          this.toastCtrl.create({
+            message: 'Votre compte à été créé',
+            duration: 3000,
+            position: 'bottom'
+          }).present();
+          this.viewCtrl.dismiss();
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            this.notification('Error', `Message : ${err.error.code} <br/> Message : ${err.error.errmsg}`);
+            console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
         }
+      );
+    } else {
+      this.notification('Information', 'Vos mots de passes ne correspondent pas');
     }
+  }
 
-    /**
-     * Return a boolean if both password are equals
-     * @param  {string}  p1 first password
-     * @param  {string}  p2 confirm password
-     * @return {boolean}    is both are equals
-     */
-    static validate_password(p1: string, p2: string): boolean {
-        return p1 === p2;
-    }
+  /**
+   * Return a boolean if both password are equals
+   * @param  {string}  p1 first password
+   * @param  {string}  p2 confirm password
+   * @return {boolean}    is both are equals
+   */
+  static validate_password(p1: string, p2: string): boolean {
+    return p1 === p2;
+  }
 
-    /**
-     * Display a notification is case of error or information
-     * @param {string} title - The title of the notification
-     * @param {string} message The message to display
-     */
-    public notification(title: string, message: string): void {
-        this.alertCtrl.create({
-            title: title,
-            subTitle: message,
-            buttons: ['OK']
-        }).present();
-    }
+  /**
+   * Display a notification is case of error or information
+   * @param {string} title - The title of the notification
+   * @param {string} message The message to display
+   */
+  public notification(title: string, message: string): void {
+    this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
+  }
 
-    ngOnInit() {
-        this.registerService.get_clubs().subscribe(
-            res => this.clubs = res,
-            error => console.log(error)
-        );
-    }
+  ngOnInit() {
+    this.registerService.get_clubs().subscribe(
+      res => this.clubs = res,
+      error => console.log(error)
+    );
+  }
 }
