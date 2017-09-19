a5ed9d82b722e19f1712e5dec34fce618f8f23ab
normandjulian <normandjulian@gmail.com>
2017-09-19 07:37:26 +0200

    enchaine

--- src/pages/create-game/create-game.ts
+++ src/pages/create-game/create-game.ts
@@ -1,7 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {DashboardService} from '../dashboard/dashboard-service';
-import {FormBuilder, Validators, FormGroup} from '@angular/forms';
-import {Game} from "../../classes/game.class";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DashboardService } from '../dashboard/dashboard-service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Game } from "../../classes/game.class";
 
 @Component({
   selector: 'app-create-game',
@@ -9,7 +9,7 @@ import {Game} from "../../classes/game.class";
   providers: [DashboardService]
 })
 
-export class CreateGamePage implements OnInit{
+export class CreateGamePage implements OnInit {
   @Input() team;
   @Output() game: EventEmitter<Game> = new EventEmitter<Game>();
   @Output() cancel: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -19,11 +19,14 @@ export class CreateGamePage implements OnInit{
   constructor(private service: DashboardService, private fb: FormBuilder) {
 
     this.game_form = this.fb.group({
-      number: ['', [<any>Validators.required]],
-      firstname: ['', [<any>Validators.required]],
-      lastname: ['', [<any>Validators.required]],
-      birthdate: ['', []],
-      license: ['', []]
+      opponent: ['', [<any>Validators.required]],
+      date: ['', [<any>Validators.required]],
+      time: ['', [<any>Validators.required]],
+      period: this.fb.group({
+        type: [4, [<any>Validators.required]],
+        time: [10, [<any>Validators.required]]
+      }),
+      home: [true, []]
     });
   }
 
